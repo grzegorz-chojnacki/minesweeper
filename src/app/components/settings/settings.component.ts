59cdaf69b8fedd7b7a4215745fc46fbc2cc59c2a
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-07-20 15:35:13 +0200

    Added settings synchronization between inputs Added difficulty presets

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { difficulties } from '../../difficulties';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  difficulties = difficulties;
+  difficultyNames = Object.keys(difficulties);
+
+  difficulty: string = this.difficultyNames[0];
+  boardDimension: number;
+  numberOfBombs: number;
+
+  // Update inputs based on selected preset, but prevent setting them to
+  // undefined if the user sets custom values
+  updateInputs() {
+    if (this.difficulty != 'Custom') {
+      this.boardDimension = difficulties[this.difficulty].boardDimension;
+      this.numberOfBombs = difficulties[this.difficulty].numberOfBombs;
+    }
+  }
+
+  // Change the difficulty of the selected preset to 'Custom',
+  // or find matching preset
+  updateSelect() {
+    for (let name of this.difficultyNames) {
+      if (this.boardDimension == difficulties[name].boardDimension &&
+        this.numberOfBombs == difficulties[name].numberOfBombs) {
+        this.difficulty = name;
+        return;
+      }
+    }
+    this.difficulty = 'Custom';
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.difficulty)
+    this.updateInputs();
   }
 
 }
---
+++ src/app/difficulties.ts
@@ -0,0 +1,18 @@
+export const difficulties = {
+  'Beginner': {
+    boardDimension: 10,
+    numberOfBombs: 10
+  },
+  'Intermediate': {
+    boardDimension: 20,
+    numberOfBombs: 50
+  },
+  'Advanced': {
+    boardDimension: 25,
+    numberOfBombs: 120
+  },
+  'Master': {
+    boardDimension: 25,
+    numberOfBombs: 180
+  }
+};
