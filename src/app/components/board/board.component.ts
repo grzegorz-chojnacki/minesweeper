abd41575d21bad2cd45a7acadd51abe06db22dbc
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-07-22 13:59:33 +0200

    Basic game board setup

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { Difficulty } from 'src/app/difficulty';
 
 @Component({
@@ -6,8 +6,22 @@ import { Difficulty } from 'src/app/difficulty';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit {
-  @Input() difficulty: Difficulty;
+export class BoardComponent implements OnInit, OnChanges {
+  @Input() difficulty: Difficulty = new Difficulty('Begginer', 10, 10);
+  board: number[];
+
+  // Controlls the size of each field on the board, in pixels
+  fieldSize = 40;
+
+  // Refresh the board after starting new game
+  ngOnChanges(changes: SimpleChanges): void {
+    this.difficulty = changes.difficulty.currentValue;
+    this.board = Array.from(Array(this.difficulty.boardDimension ** 2).keys());
+  }
+
+  getCSSGridTemplate(): string {
+    return `repeat(${this.difficulty.boardDimension}, auto)`;
+  }
 
   constructor() { }
 
