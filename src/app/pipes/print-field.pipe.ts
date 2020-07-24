ca7a692358d0e9a94eb76b77786b0225a5c0f56d
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-07-24 13:40:19 +0200

    Added variable size material icons

--- src/app/pipes/print-field.pipe.ts
+++ src/app/pipes/print-field.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 import { Field } from '../field';
 
 @Pipe({
@@ -6,30 +7,43 @@ import { Field } from '../field';
   pure: false
 })
 export class PrintFieldPipe implements PipeTransform {
+  private static readonly bombIcon = 'gps_fixed';
+  private static readonly flagIcon = 'tour</span>';
 
-  // When the field has been checked and you can view its contents
-  private asChecked(field: Field): string {
+  private buildIcon(iconName: string, fontSize: number): string {
+    return `<span class="material-icons"
+              style="font-size: ${fontSize}px;">
+              ${iconName}
+            </span>`;
+  }
+
+  constructor(private domSanitizer: DomSanitizer) { }
+
+  // Used when the field has been checked and you can view its contents
+  private asChecked(field: Field, fontSize: number): string | SafeHtml {
     switch (field.getValue()) {
       case Field.bomb:
-        return 'B';
+        return this.domSanitizer.bypassSecurityTrustHtml(
+          this.buildIcon(PrintFieldPipe.bombIcon, fontSize));
       case Field.clear:
-        return ' ';
+        return '';
       default:
         return field.getValue().toString();
     }
   }
 
-  // When the field hasn't been checked and you can only view  if it's flagged
-  private asNotChecked(field: Field): string {
+  // Used when the field hasn't been checked and you can only view if it's flagged
+  private asNotChecked(field: Field, fontSize: number): string | SafeHtml {
     return (field.isFlagged())
-      ? 'F'
-      : ' ';
+      ? this.domSanitizer.bypassSecurityTrustHtml(
+          this.buildIcon(PrintFieldPipe.flagIcon, fontSize))
+      : '';
   }
 
-  transform(field: Field): string {
+  transform(field: Field, fontSize: number): string | SafeHtml {
     return (field.isChecked())
-      ? this.asChecked(field)
-      : this.asNotChecked(field);
+      ? this.asChecked(field, fontSize)
+      : this.asNotChecked(field, fontSize);
   }
 
 }
