6ed6327a7eb3a1f0d1524f4a9c45eed29476f1b7
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-08-24 19:30:47 +0200

    Added tests for DifficultyService

--- src/app/services/difficulty.service.spec.ts
+++ src/app/services/difficulty.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DifficultyService } from './difficulty.service';
+import { Difficulty, difficulties } from '../difficulty';
 
 describe('DifficultyService', () => {
-  let service: DifficultyService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(DifficultyService);
+    window.localStorage.clear();
   });
 
   it('should be created', () => {
+    const service = TestBed.inject(DifficultyService);
     expect(service).toBeTruthy();
+  });
+
+  it('should initialize when local storage is empty', () => {
+    const service = TestBed.inject(DifficultyService);
+
+    let result: Difficulty;
+    service.difficulty.subscribe(difficulty => result = difficulty);
+
+    expect(result).toBe(difficulties[0]);
+  });
+
+  it('should load data when local storage is not empty', () => {
+    const newDifficulty = new Difficulty('', 5, 5);
+    window.localStorage.setItem('difficulty', JSON.stringify(newDifficulty));
+
+    const service = TestBed.inject(DifficultyService);
+
+    let result: Difficulty;
+    service.difficulty.subscribe(difficulty => result = difficulty);
+
+    // Compare only members, not object type
+    // (which for some reason is different)
+    expect({...result}).toEqual({...newDifficulty});
+  });
+
+  it('should set and update new difficulty value', () => {
+    const service = TestBed.inject(DifficultyService);
+    const newDifficulty = new Difficulty('', 5, 5);
+
+    service.newDifficulty(newDifficulty);
+
+    let result: Difficulty;
+    service.difficulty.subscribe(difficulty => result = difficulty);
+
+    expect(result).toEqual(newDifficulty);
   });
 });
