f127c8093866c47495599fd7cec0310fedfae2fc
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-08-24 20:28:57 +0200

    Added tests for SettingsService

--- src/app/services/settings.service.spec.ts
+++ src/app/services/settings.service.spec.ts
@@ -1,16 +1,85 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SettingsService } from './settings.service';
+import { Subscription } from 'rxjs';
 
 describe('SettingsService', () => {
-  let service: SettingsService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(SettingsService);
+    window.localStorage.clear();
   });
 
   it('should be created', () => {
+    const service = TestBed.inject(SettingsService);
     expect(service).toBeTruthy();
+  });
+
+  // Field size
+
+  it('should initialize fieldSize when local storage is empty', () => {
+    const service = TestBed.inject(SettingsService);
+
+    let fieldSize: number;
+    service.fieldSize.subscribe(fs => fieldSize = fs).unsubscribe();
+
+    expect(fieldSize).toBe(service.minFieldSize);
+  });
+
+  it('should load fieldSize when local storage is not empty', () => {
+    const savedFieldSize = 32;
+    localStorage.setItem('fieldSize', savedFieldSize.toString());
+
+    const service = TestBed.inject(SettingsService);
+    let fieldSize: number;
+    service.fieldSize.subscribe(fs => fieldSize = fs).unsubscribe();
+
+    expect(fieldSize).toBe(savedFieldSize);
+  });
+
+  it('should set and update new fieldSize value', () => {
+    const service = TestBed.inject(SettingsService);
+    const newFieldSize = 32;
+
+    let fieldSize: number;
+    service.setFieldSize(newFieldSize);
+    service.fieldSize.subscribe(fs => fieldSize = fs).unsubscribe();
+
+    expect(fieldSize).toBe(newFieldSize);
+  });
+
+  // Sidenav auto hide
+
+  it('should initialize sidenavAutoHide when local storage is empty', () => {
+    const service = TestBed.inject(SettingsService);
+
+    let sidenavAutoHide: boolean;
+    service.sidenavAutoHide.subscribe(sah => sidenavAutoHide = sah)
+      .unsubscribe();
+
+    expect(sidenavAutoHide).toBe(service.defaultSidenavAutoHide);
+  });
+
+  it('should load sidenavAutoHide when local storage is not empty', () => {
+    const savedValue = false;
+    localStorage.setItem('sidenavAutoHide', savedValue.toString());
+
+    const service = TestBed.inject(SettingsService);
+    let sidenavAutoHide: boolean;
+    service.sidenavAutoHide.subscribe(sah => sidenavAutoHide = sah)
+      .unsubscribe();
+
+    expect(sidenavAutoHide).toBe(savedValue);
+  });
+
+  it('should set and update new sidenavAutoHide value', () => {
+    const service = TestBed.inject(SettingsService);
+    const newValue = false;
+
+    let sidenavAutoHide: boolean;
+    service.setSidenavAutoHide(newValue);
+    service.sidenavAutoHide.subscribe(sah => sidenavAutoHide = sah)
+      .unsubscribe();
+
+    expect(sidenavAutoHide).toBe(newValue);
   });
 });
