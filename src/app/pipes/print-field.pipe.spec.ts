9c05c790094302a38a5673153d24c6257dd43efb
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-08-24 13:58:57 +0200

    Added better tests for PrintFieldPipe

--- src/app/pipes/print-field.pipe.spec.ts
+++ src/app/pipes/print-field.pipe.spec.ts
@@ -1,8 +1,47 @@
 import { PrintFieldPipe } from './print-field.pipe';
 
 describe('PrintFieldPipe', () => {
-  it('create an instance', () => {
+  it('should create an instance', () => {
     const pipe = new PrintFieldPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should output nothing if field is clear', () => {
+    const pipe = new PrintFieldPipe();
+    const fields = [
+      { value: 0, flagged: false, checked: false },
+      { value: 0, flagged: false, checked: true },
+    ];
+
+    fields.forEach(field => {
+      const result = pipe.transform(field.value, field.flagged, field.checked);
+      expect(result).toBe('');
+    });
+  });
+
+  it('should output nothing if field is not checked or flagged', () => {
+    const pipe = new PrintFieldPipe();
+    const field = { value: 7, flagged: false, checked: false };
+
+    const result = pipe.transform(field.value, field.flagged, field.checked);
+    expect(result).toBe('');
+  });
+
+  it('should output correct markup if field is flagged', () => {
+    const pipe = new PrintFieldPipe();
+    const field = { value: 0, flagged: true, checked: false };
+
+    const result = pipe.transform(field.value, field.flagged, field.checked);
+    expect(result).toBe('<span class="material-icons field-icon">tour</span>');
+  });
+
+  it('should output correct markup if field is checked and is a bomb', () => {
+    const pipe = new PrintFieldPipe();
+    const field = { value: 7, flagged: false, checked: true };
+
+    const result = pipe.transform(field.value, field.flagged, field.checked);
+    expect(result).toBe(
+      `<span class="color-${field.value}">${field.value}</span>`
+    );
   });
 });
