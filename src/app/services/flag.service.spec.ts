304998e544c6f666e46d0064ff5c2f3152f70e4b
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-08-25 21:14:25 +0200

    Added tests for flagService

--- src/app/services/flag.service.spec.ts
+++ src/app/services/flag.service.spec.ts
@@ -3,14 +3,24 @@ import { TestBed } from '@angular/core/testing';
 import { FlagService } from './flag.service';
 
 describe('FlagsService', () => {
-  let service: FlagService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FlagService);
   });
 
   it('should be created', () => {
+    const service = TestBed.inject(FlagService);
     expect(service).toBeTruthy();
+  });
+
+  it('should set and and update new flag count', () => {
+    const service = TestBed.inject(FlagService);
+    const newFlagCount = 7;
+
+    let result: number;
+    const subscription = service.counter.subscribe(count => result = count);
+    service.setFlags(newFlagCount);
+    subscription.unsubscribe();
+
+    expect(result).toBe(newFlagCount);
   });
 });
