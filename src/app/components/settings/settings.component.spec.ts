6df60a581fe566a95e20b2441d0f1834fc22a6d5
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-09-06 16:10:19 +0200

    Added stubs to spec

--- src/app/components/settings/settings.component.spec.ts
+++ src/app/components/settings/settings.component.spec.ts
@@ -10,6 +10,27 @@ import { SettingsComponent } from './settings.component';
 import { SettingsService } from 'src/app/services/settings.service';
 import { DifficultyService } from 'src/app/services/difficulty.service';
 import { BombPercentagePipe } from 'src/app/pipes/bomb-percentage.pipe';
+import { Difficulty } from 'src/app/classes/difficulty';
+import { BehaviorSubject } from 'rxjs';
+
+class SettingsServiceStub implements Partial<SettingsService> {
+  public readonly minFieldSize = 30;
+  public readonly maxFieldSize = 60;
+  public fieldSize = new BehaviorSubject<number>(this.minFieldSize);
+  public sidenavAutoHide = new BehaviorSubject<boolean>(true);
+
+  public setFieldSize = (n: number): void => this.fieldSize.next(n);
+  public setSidenavAutoHide = (b: boolean): void =>
+    this.sidenavAutoHide.next(b)
+}
+const settingsServiceStub = new SettingsServiceStub();
+
+class DifficultyServiceStub implements Partial<DifficultyService> {
+  public initialDifficulty = new Difficulty(7, 7);
+  public difficulty = new BehaviorSubject<Difficulty>(this.initialDifficulty);
+  public newDifficulty = (d: Difficulty): void => this.difficulty.next(d);
+}
+const difficultyServiceStub = new DifficultyServiceStub();
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -30,8 +51,8 @@ describe('SettingsComponent', () => {
       ],
       providers: [
         { provide: FormBuilder },
-        { provide: SettingsService },
-        { provide: DifficultyService }
+        { provide: SettingsService, useValue: settingsServiceStub },
+        { provide: DifficultyService, useValue: difficultyServiceStub }
       ]
     }).compileComponents();
   }));
