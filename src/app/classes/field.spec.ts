24e01ac44fa15c4adcc5e09ec9bb8983890bdfad
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-09-06 15:36:03 +0200

    Added test for field matrix generation

--- src/app/classes/field.spec.ts
+++ src/app/classes/field.spec.ts
@@ -1,13 +1,15 @@
 import { Field } from './field';
 
 describe('Field', () => {
-  it('should apply function around itself in 2d field matrix', () => {
-    const fields = [
-      [new Field(0, 0), new Field(1, 0), new Field(2, 0)],
-      [new Field(0, 1), new Field(1, 1), new Field(2, 1)],
-      [new Field(0, 2), new Field(1, 2), new Field(2, 2)]
-    ];
-    const middleField = fields[1][1];
+  it('should generate field matrix of proper size', () => {
+    const fields = Field.makeMatrix(10);
+    expect(fields.length).toBe(10);
+    fields.every(row => expect(row.length).toBe(fields.length));
+  });
+
+  it('should apply function around itself in field matrix', () => {
+    const fields = Field.makeMatrix(5);
+    const middleField = fields[2][2];
 
     middleField.applyAround(fields, field => field.value = 1);
     expect(middleField.value).not.toBe(1);
