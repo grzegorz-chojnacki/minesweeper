f03c03131998c8a13f16976d97ae0ec07e03b105
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-09-12 17:44:45 +0200

    Categorized tests

--- src/app/services/settings.service.spec.ts
+++ src/app/services/settings.service.spec.ts
@@ -6,80 +6,86 @@ function makeServiceWithEmptyStorage(): SettingsService {
 }
 
 describe('SettingsService', () => {
-  it('should be created', () => {
-    const service = makeServiceWithEmptyStorage();
-    expect(service).toBeTruthy();
-  });
+  describe('Initialization behaviour', () => {
+    it('should be created', () => {
+      const service = makeServiceWithEmptyStorage();
+      expect(service).toBeTruthy();
+    });
 
-  it('should initialize fieldSize when local storage is empty', () => {
-    const service = makeServiceWithEmptyStorage();
+    it('should initialize fieldSize when local storage is empty', () => {
+      const service = makeServiceWithEmptyStorage();
 
-    service.fieldSize.subscribe(fieldSize =>
-      expect(fieldSize).toBe(service.minFieldSize)
-    ).unsubscribe();
-  });
-
-  it('should load fieldSize when local storage is not empty', () => {
-    const savedFieldSize = 32;
-    const storage = new FakeStorage();
-    storage.setItem('fieldSize', savedFieldSize.toString());
-
-    const service = new SettingsService(storage);
-
-    service.fieldSize.subscribe(fieldSize =>
-      expect(fieldSize).toBe(savedFieldSize)
-    ).unsubscribe();
-  });
-
-  it('should set and update new fieldSize value', () => {
-    const service = makeServiceWithEmptyStorage();
-    const newFieldSize = 32;
-
-    service.setFieldSize(newFieldSize);
-
-    service.fieldSize.subscribe(fieldSize =>
-      expect(fieldSize).toBe(newFieldSize)
-    ).unsubscribe();
-  });
-
-  it('should not allow for fieldSize to be set out of its bounds', () => {
-    const service = makeServiceWithEmptyStorage();
-    const invalidValues = [service.minFieldSize - 1, service.maxFieldSize + 1];
-
-    for (const value of invalidValues) {
-      service.setFieldSize(value);
       service.fieldSize.subscribe(fieldSize =>
-        expect(fieldSize).not.toBe(value)
+        expect(fieldSize).toBe(service.minFieldSize)
       ).unsubscribe();
-    }
+    });
+
+    it('should load fieldSize when local storage is not empty', () => {
+      const savedFieldSize = 32;
+      const storage = new FakeStorage();
+      storage.setItem('fieldSize', savedFieldSize.toString());
+
+      const service = new SettingsService(storage);
+
+      service.fieldSize.subscribe(fieldSize =>
+        expect(fieldSize).toBe(savedFieldSize)
+      ).unsubscribe();
+    });
+
+    it('should initialize sidenavAutoHide when local storage is empty', () => {
+      const service = makeServiceWithEmptyStorage();
+
+      service.sidenavAutoHide.subscribe(sidenavAutoHide =>
+        expect(sidenavAutoHide).toBe(service.defaultSidenavAutoHide)
+      ).unsubscribe();
+    });
+
+    it('should load sidenavAutoHide when local storage is not empty', () => {
+      const savedValue = false;
+      const storage = new FakeStorage();
+      storage.setItem('sidenavAutoHide', savedValue.toString());
+
+      const service = new SettingsService(storage);
+      service.sidenavAutoHide.subscribe(sidenavAutoHide =>
+        expect(sidenavAutoHide).toBe(savedValue)
+      ).unsubscribe();
+    });
   });
 
-  it('should initialize sidenavAutoHide when local storage is empty', () => {
-    const service = makeServiceWithEmptyStorage();
+  describe('Field size behaviour', () => {
+    it('should set and update new fieldSize value', () => {
+      const service = makeServiceWithEmptyStorage();
+      const newFieldSize = 32;
 
-    service.sidenavAutoHide.subscribe(sidenavAutoHide =>
-      expect(sidenavAutoHide).toBe(service.defaultSidenavAutoHide)
-    ).unsubscribe();
+      service.setFieldSize(newFieldSize);
+
+      service.fieldSize.subscribe(fieldSize =>
+        expect(fieldSize).toBe(newFieldSize)
+      ).unsubscribe();
+    });
+
+    it('should not allow for fieldSize to be set out of its bounds', () => {
+      const service = makeServiceWithEmptyStorage();
+      const invalidValues = [service.minFieldSize - 1, service.maxFieldSize + 1];
+
+      for (const value of invalidValues) {
+        service.setFieldSize(value);
+        service.fieldSize.subscribe(fieldSize =>
+          expect(fieldSize).not.toBe(value)
+        ).unsubscribe();
+      }
+    });
   });
 
-  it('should load sidenavAutoHide when local storage is not empty', () => {
-    const savedValue = false;
-    const storage = new FakeStorage();
-    storage.setItem('sidenavAutoHide', savedValue.toString());
+  describe('Sidenav autohide behaviour', () => {
+    it('should set and update new sidenavAutoHide value', () => {
+      const service = makeServiceWithEmptyStorage();
+      const newValue = false;
 
-    const service = new SettingsService(storage);
-    service.sidenavAutoHide.subscribe(sidenavAutoHide =>
-      expect(sidenavAutoHide).toBe(savedValue)
-    ).unsubscribe();
-  });
-
-  it('should set and update new sidenavAutoHide value', () => {
-    const service = makeServiceWithEmptyStorage();
-    const newValue = false;
-
-    service.setSidenavAutoHide(newValue);
-    service.sidenavAutoHide.subscribe(sidenavAutoHide =>
-    expect(sidenavAutoHide).toBe(newValue)
-    ).unsubscribe();
+      service.setSidenavAutoHide(newValue);
+      service.sidenavAutoHide.subscribe(sidenavAutoHide =>
+      expect(sidenavAutoHide).toBe(newValue)
+      ).unsubscribe();
+    });
   });
 });
