f13ca11a6d7b193ef5783767f7c21b5419bfa7f4
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-09-13 14:16:10 +0200

    Linting

--- src/app/services/fakeStorage.ts
+++ src/app/services/fakeStorage.ts
@@ -1,20 +1,26 @@
 export class FakeStorage implements Storage {
-  private storage: {[key: string]: string} = {};
+  private storage: { [key: string]: string } = {};
+
   public get length(): number {
     return Object.keys(this.storage).length;
   }
+
   public getItem(key: string): string {
     return (key in this.storage) ? this.storage[key] : null;
   }
+
   public setItem(key: string, value: string): void {
     this.storage[key] = value;
   }
+
   public key(index: number): string {
     return Object.keys(this.storage)[index];
   }
+
   public removeItem(key: string): void {
     delete this.storage[key];
   }
+
   public clear = () => {
     this.storage = {};
   }
