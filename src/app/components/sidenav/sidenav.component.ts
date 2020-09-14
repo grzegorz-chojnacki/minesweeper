9cc7ef66adf507d7a482b294548c6be9dd680583
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-09-14 17:55:54 +0200

    Added SidenavComponent file

---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+  @Output() public passThrough = new EventEmitter();
+
+  constructor() { }
+  ngOnInit(): void { }
+}
