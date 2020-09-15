44240e3894e5d5bad74c44fe6548a0f1ca6369ce
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-09-15 15:09:36 +0200

    Added material element harnesses to tests. Added more template tests

--- src/app/components/settings/settings.component.spec.ts
+++ src/app/components/settings/settings.component.spec.ts
@@ -1,8 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCheckboxHarness } from '@angular/material/checkbox/testing';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatSliderHarness } from '@angular/material/slider/testing';
 
 import { SettingsComponent } from './settings.component';
 import { SettingsService } from 'src/app/services/settings.service';
@@ -11,6 +15,7 @@ import { FakeStorage } from 'src/app/services/fakeStorage';
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async(() => {
     const settingsService = new SettingsService(new FakeStorage());
@@ -28,6 +33,7 @@ describe('SettingsComponent', () => {
     }).compileComponents()
       .then(() => {
         fixture = TestBed.createComponent(SettingsComponent);
+        loader = TestbedHarnessEnvironment.loader(fixture);
         component = fixture.componentInstance;
     });
   }));
@@ -46,19 +52,6 @@ describe('SettingsComponent', () => {
       expect(component.fieldSize).toBe(42);
     });
 
-    it('should init field size slider value', () => {
-      const settingsService = TestBed.inject(SettingsService);
-      settingsService.setFieldSize(32);
-      fixture.detectChanges();
-
-      component.ngOnInit();
-
-      const slider = fixture.debugElement
-        .query(By.css('mat-slider')).attributes;
-
-      expect(slider['ng-reflect-value']).toBe('32');
-    });
-
     it('should init sidenav auto hide setting', () => {
       const settingsService = TestBed.inject(SettingsService);
       settingsService.setSidenavAutoHide(false);
@@ -70,6 +63,39 @@ describe('SettingsComponent', () => {
   });
 
   describe('Template behaviour', () => {
+    it('should init field size slider value', async () => {
+      const settingsService = TestBed.inject(SettingsService);
+      const fieldSize = 32;
+      settingsService.setFieldSize(fieldSize);
+
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      const slider = await loader.getHarness(MatSliderHarness);
+      expect(await slider.getValue()).toBe(fieldSize);
+    });
+
+    it('should set slider value bounds', async () => {
+      const settingsService = TestBed.inject(SettingsService);
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      const slider = await loader.getHarness(MatSliderHarness);
+      expect(await slider.getMaxValue()).toBe(settingsService.maxFieldSize);
+      expect(await slider.getMinValue()).toBe(settingsService.minFieldSize);
+    });
+
+    it('should set sidenav auto hide checkbox', async () => {
+      const settingsService = TestBed.inject(SettingsService);
+      settingsService.setSidenavAutoHide(true);
+
+      component.ngOnInit();
+      fixture.detectChanges();
+
+      const checkbox = await loader.getHarness(MatCheckboxHarness);
+      expect(await checkbox.isChecked()).toBe(true);
+    });
+
     it('should show field px size, in field size label', () => {
       const settingsService = TestBed.inject(SettingsService);
       settingsService.setFieldSize(31);
